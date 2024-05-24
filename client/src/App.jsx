5963353880195046cfd77db2dd3774ab1b58fdf7
Anas-Bigscal <anas@bigscal.com>
2024-05-24 18:30:08 +0530

    in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React,{ useEffect, useMemo, useState }  from 'react';
 import {io} from 'socket.io-client';
+import {Button, Container, TextField, Typography} from '@mui/material';
 
 const App = () => {
-  const socket = io("http://localhost:8000");
-  return  <div>App</div>
+
+  // const socket = io("http://localhost:8000/");
+  const socket = useMemo(()=> io("http://localhost:8000/"), []);
+
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", {message, room});
+    setMessage("");
+  }
+
+  useEffect(()=>{
+    socket.on("connect", ()=>{
+      setSocketId(socket.id);
+      console.log("CONNECTED CLIENT",socket.id);
+     });
+
+     socket.on("receive-message", (data) =>{
+      console.log("MESSAGE RECEIVED ===>>",data);
+     });
+
+     socket.on("WELCOME", (s)=>{
+      console.log(s);
+     });
+
+     return () => {
+      socket.disconnect();
+     }
+  },[])
+
+  return <Container maxWidth="sm">
+    <Typography variant='h1' component="div" gutterBottom>
+      Welcome to socket.io
+    </Typography>
+
+    <Typography variant='h5' component="div" gutterBottom>
+      {socketId}
+    </Typography>
+
+    <form onSubmit={handleSubmit}>
+      <TextField value={message} onChange={e => setMessage(e.target.value)} id="outlined-basic" label="Message" variant='outlined'/>
+      <TextField value={room} onChange={e => setRoom(e.target.value)} id="outlined-basic" label="Room" variant='outlined'/>
+      <Button type="submit" variant='contained' color='primary'>Send</Button>
+    </form>
+  </Container>
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -3,32 +3,41 @@ import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from 'cors';
 
-const app = express();
 const port = 8000;
+const app = express();
 
 const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173/",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
         credentials: true,
     }
 });
 
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
     credentials: true,
 }));
 
-app.get('/',(req, res) => {
+app.get("/",(req, res) => {
     res.send("HEllooooooo");
-})
+});
 
 io.on("connection", (socket) =>{
-    console.log("USER CONNECTED");
-    console.log("ID",socket.id);
+    console.log("USER CONNECTED",socket.id);
+    // console.log("ID",socket.id);
+    // socket.emit("WELCOME",`WELCOME TO THE DEATH`);
+    // socket.broadcast.emit("WELCOME",`${socket.id} will face DEATH`);
+    socket.on("message", ({room, message})=>{
+        console.log({room, message});
+        io.to(room).emit("receive-message",message);
+    })
+    socket.on("disconnect",()=>{
+        console.log("USER Disconnected",socket.id);
+    })
 });
 
 server.listen(port , () => {
